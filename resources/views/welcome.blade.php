79f26915ad85dafab7219da7505f07711c4d2b02
joserhond <joserhond@hotmail.com>
2018-05-02 16:51:12 -0500

    verification tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>High Power Brake</title>
         <meta name="description" content="High Power Brake se especializa en repuestos para el sistema de frenado. Contamos con pastillas de freno y zapatas de freno aprobadas en certificaciones nacionales e internacionales que le brindarán una mejor experiencia de manejo." />
+        <meta name="google-site-verification" content="MfJZDcn3_sycaE1Nq_-JQgoAfaLix8uoskadZYozjdc" />
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
 
